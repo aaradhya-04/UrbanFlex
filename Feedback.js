983cb543d74ed71599333cb14b2a598a8c2952e8
aaradhya-04 <aaradhyamittal77@gmail.com>
2025-01-29 21:21:06 +0530

    Updated project: removed old files and added new ones

--- Feedback.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("feedback-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-    document.getElementById("thank-you-message").classList.remove("hidden"); // Show thank you message
-    this.reset(); // Reset the form
-});
